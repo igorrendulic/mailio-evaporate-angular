8d99d83d6cbfe6fe1d9d00ab764b3993cb7b45bc
Igor Rendulic <igor@mail.io>
2021-11-12 09:48:57 -0700

    Adde golang presign server

--- presignservers/go/go.mod
+++ presignservers/go/go.mod
@@ -2,4 +2,8 @@ module github.com/igorrendulic/mailio-evaporate-angular/presignservers/go
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/joho/godotenv v1.4.0
+	github.com/rs/zerolog v1.26.0
+)
--- presignservers/go/main.go
+++ presignservers/go/main.go
@@ -4,8 +4,13 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -17,37 +22,61 @@ var (
 
 var db = make(map[string]string)
 
-func setupRouter() *gin.Engine {
+func setupRouter(awsSecret string) *gin.Engine {
 	r := gin.Default()
 	// Ping test
 	r.GET("/sign_auth", func(c *gin.Context) {
-		c.JSON(200, gin.H{"pong": "pong"})
+		qs := c.Query("to_sign")
+
+		strs := strings.Split(qs, "\n")
+		if len(strs) <= 1 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "to_sign is invalid"})
+			return
+		}
+		data := strings.Split(strs[2], "/")
+		date, regionName, serviceName, requestName = data[0], data[1], data[2], data[3]
+
+		log.Info().Msgf("date: %s, regionName: %s, serviceName: %s, requestName: %s", date, regionName, serviceName, requestName)
+
+		signedKey := signature(date, qs, awsSecret)
+
+		c.Data(http.StatusOK, "application/octet-stream", []byte(signedKey))
 	})
 
 	return r
 }
 
-func signature(t, sts string) string {
-	h := HMAC(derivedKey(t), []byte(sts))
+func signature(t, sts string, awsSecretKey string) string {
+	h := signHmac(derivedKey(t, awsSecretKey), []byte(sts))
 	return fmt.Sprintf("%x", h)
 }
 
-func derivedKey(t string) []byte {
-	h := HMAC([]byte("AWS4"+"AWS_SECRET"), []byte(t))
-	h = HMAC(h, []byte(regionName))
-	h = HMAC(h, []byte(serviceName))
-	h = HMAC(h, []byte(requestName))
+func derivedKey(t string, awsSecretKey string) []byte {
+	h := signHmac([]byte("AWS4"+awsSecretKey), []byte(t))
+	h = signHmac(h, []byte(regionName))
+	h = signHmac(h, []byte(serviceName))
+	h = signHmac(h, []byte(requestName))
 	return h
 }
 
-func HMAC(key, data []byte) []byte {
+// HMAC
+func signHmac(key, data []byte) []byte {
 	h := hmac.New(sha256.New, key)
 	h.Write(data)
 	return h.Sum(nil)
 }
 
 func main() {
-	r := setupRouter()
+	err := godotenv.Load("../.env")
+	awsSecret := os.Getenv("AWS_SECRET")
+	if err != nil {
+		log.Fatal().Msgf("Error loading .env file", err)
+	}
+	if awsSecret == "" {
+		log.Fatal().Msg("AWS_SECRET is empty")
+	}
+
+	r := setupRouter(awsSecret)
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
